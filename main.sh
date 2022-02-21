4d0433da50ed4ba02b22d533d12dcfb9ac627008
Chaol Westfall <chaolw3stf4ll@protonmail.com>
2022-02-21 03:01:02 -0600

    Corrigieron errores y aplicaron mejores practicas

--- main.sh
+++ main.sh
@@ -16,16 +16,16 @@ function descarga_Git {
         echo
 	if [ -f githubtools.lst ]; then
 		echo "Se encontro la list descargas, comenzando..."
-		for GITREPO in `grep -v "^#" githubtools.lst`;
+		for GITREPO in $(grep -v "^#" githubtools.lst);
 		do
-			NOMBRE_REPO=`sed 's|https://git.*.com/.*/||' <<< $GITREPO`
-			SANITIZADO=`sed 's|\.git||' <<< $NOMBRE_REPO`
+			NOMBRE_REPO=$(sed 's|https://git.*.com/.*/||' <<< "$GITREPO")
+			SANITIZADO=$(sed 's|\.git||' <<< "$NOMBRE_REPO")
             descarga_Actualiza
 		done
 	else
 		echo
 		echo "No puedo encontrar la lista de descarga! No puedo hacer nada!"
-		echo "Asegurate que el archivo githubtools.lst este en le mismo directorio que este script!"
+		echo "Asegurate que el archivo githubtools.lst este en le mismo directorio que este script!: $DIRACTUAL"
 	fi
 }
 
@@ -33,12 +33,12 @@ function descarga_Actualiza {
         if [ -d "/opt/$SANITIZADO" ]; then
                 echo
                 echo "Actualizando $SANITIZADO ..."
-                cd /opt/$SANITIZADO
+                cd /opt/"$SANITIZADO"
                 git pull
         else
                 echo
                 echo "Descargando $SANITIZADO .."
-                sudo git -C /opt/ clone $GITREPO
+                sudo git -C /opt/ clone "$GITREPO"
         fi
 }
 
@@ -47,16 +47,15 @@ function actualizar_SO {
 }
 
 main () {
-        USUARIO=`whoami`
-        DIRACTUAL=`pwd`
+        USUARIO=$(whoami)
+        DIRACTUAL=$(pwd)
         banner
-        if [ `id -u` -e 0 ]; then
+        if [ "$(id -u)" -eq 0 ]; then
             echo "NO me executes como root, te lo pedire cuando lo necesite..."
             exit
         else
             echo "Revisando conexión a internet..."
-            ping -c1 google.com > /dev/null 2>&1
-      	    if [ "$?" != 0 ]; then
+      	    if ! ping -c1 google.com > /dev/null 2>&1; then
       		         echo "No hay internet! Revisa tu conexión!"
       	    else
       		         echo "Tienes conexión, continuando..."
@@ -66,7 +65,7 @@ main () {
                      echo
                      echo "Voy a intentar descargar las herramientas de github, estudio puede tardar un rato."
                      descarga_Git
-                     sudo chown -R $USUARIO:$USUARIO /opt/*
+                     sudo chown -R "$USUARIO":"$USUARIO" /opt/*
                      echo
                      echo "Feliz caza!"
       	    fi
